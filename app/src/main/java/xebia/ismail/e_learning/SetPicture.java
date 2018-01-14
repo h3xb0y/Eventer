2f28b989d97a1e3d52fc807b335ce34b67b3e4c7
h3xb0y <qommentator@gmail.com>
2018-01-14 22:31:46 +0200

    2k18 update

---
+++ app/src/main/java/xebia/ismail/e_learning/SetPicture.java
@@ -0,0 +1,38 @@
+package xebia.ismail.e_learning;
+
+
+public class SetPicture {
+    static int pict;
+
+
+    public static int SetPicture (int n){
+
+        switch (n) {
+            case 0:  pict = R.drawable.a0; break;
+            case 1:  pict = R.drawable.a1; break;
+            case 2:  pict = R.drawable.a2; break;
+            case 3:  pict = R.drawable.a3; break;
+            case 4:  pict = R.drawable.a4; break;
+            case 5:  pict = R.drawable.a5; break;
+            case 6:  pict = R.drawable.a6; break;
+            case 7:  pict = R.drawable.a7; break;
+            case 8:  pict = R.drawable.a8; break;
+            case 9:  pict = R.drawable.a9; break;
+            case 10: pict = R.drawable.a10; break;
+            case 11: pict = R.drawable.a11; break;
+            case 12: pict = R.drawable.a12; break;
+            case 13: pict = R.drawable.a13; break;
+            case 14: pict = R.drawable.a14; break;
+            case 15: pict = R.drawable.a15; break;
+            case 16: pict = R.drawable.a16; break;
+            case 17: pict = R.drawable.a17; break;
+            case 18: pict = R.drawable.a18; break;
+            case 19: pict = R.drawable.a19; break;
+            case 20: pict = R.drawable.a20; break;
+            case 21: pict = R.drawable.a21; break;
+            case 22: pict = R.drawable.a22; break;
+            case 23: pict = R.drawable.a23; break;
+        }
+        return pict;
+    }
+}
